8246ac0fd4f7223740ffb38623c5347e3adc3ce4
Misha Drohomiretskiy <drohomiretskiy@gmail.com>
2017-06-16 00:04:50 +0300

    Відлагоджено роботу блоку saidebar

---
+++ www/js/script.js
@@ -0,0 +1,7 @@
+ function showSaidebar() {
+ 	document.getElementById('login-fb').style.display = 'none';
+ 	document.getElementById('login-vk').style.display = 'block';
+	 document.getElementById('other-reg').style.display = 'none';
+	 document.getElementById('email-reg').style.display = 'block';
+	 document.getElementById('saidebar').style.height = '350px';
+ }
