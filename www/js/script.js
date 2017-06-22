1734036df0ee8c35e32cc952badcaa2540a42b7f
Misha Drohomiretskiy <drohomiretskiy@gmail.com>
2017-06-22 20:18:56 +0300

    Додано функціонал в кнопки футера

--- www/js/script.js
+++ www/js/script.js
@@ -1,12 +1,37 @@
  function showSaidebar() {
  	document.getElementById('login-fb').style.display = 'none';
  	document.getElementById('login-vk').style.display = 'block';
-	 document.getElementById('other-reg').style.display = 'none';
-	 document.getElementById('email-reg').style.display = 'block';
-	 document.getElementById('saidebar').style.height = '350px';
+ 	document.getElementById('other-reg').style.display = 'none';
+ 	document.getElementById('email-reg').style.display = 'block';
+ 	document.getElementById('saidebar').style.height = '350px';
  }
-function authorization(){
-		document.getElementById('saidebar-log').style.display = 'none';
-		document.getElementById('saidebar-log2').style.display = 'block';
-	
-}
+
+ function authorization() {
+ 	document.getElementById('saidebar-log').style.display = 'none';
+ 	document.getElementById('saidebar-log2').style.display = 'block';
+
+ }
+
+ function aboutContent() {
+ 	document.getElementById('main-page').style.display = 'none';
+ 	document.getElementById('contact-page').style.display = 'none';
+ 	document.getElementById('confid-page').style.display = 'none';
+ 	document.getElementById('about-page').style.display = 'block';
+
+ }
+
+ function contactContent() {
+ 	document.getElementById('main-page').style.display = 'none';
+ 	document.getElementById('about-page').style.display = 'none';
+ 	document.getElementById('confid-page').style.display = 'none';
+ 	document.getElementById('contact-page').style.display = 'block';
+
+ }
+
+ function contactConfid() {
+ 	document.getElementById('main-page').style.display = 'none';
+ 	document.getElementById('about-page').style.display = 'none';
+ 	document.getElementById('contact-page').style.display = 'none';
+ 	document.getElementById('confid-page').style.display = 'block';
+
+ }
